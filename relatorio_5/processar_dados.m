3114cf60ec665405a397613e9dc1574159a6149d
Jose Pedro <1jpsneto@gmail.com>
2015-11-26 16:18:20 -0200

    grafico dos dados pronto

--- relatorio_5/processar_dados.m
+++ relatorio_5/processar_dados.m
@@ -6,13 +6,15 @@ dados_ruido_fundo = textread('Dados_experimento_5/dados_ruido_fundo.txt');
 dados_medicao = textread('Dados_experimento_5/dados_medicao.txt');
 frequencias = dados_ruido_fundo(:, 1);
 % dados do ruido de fundo
-nivel_pressao_geradora_fundo = 10*log10(dados_ruido_fundo(:, 2)/(2e-5)^2);
-nivel_pressao_receptora_fundo = 10*log10(dados_ruido_fundo(:, 4)/(2e-5)^2);
+nivel_pressao_geradora_fundo = 20*log10(dados_ruido_fundo(:, 2)/(2e-5));
+nivel_pressao_receptora_fundo = 20*log10(dados_ruido_fundo(:, 4)/(2e-5));
 aceleracao_fundo = dados_ruido_fundo(:, 6);
 % dados da medicao
-nivel_pressao_geradora_medicao = 10*log10(dados_medicao(:, 2)/(2e-5)^2);
-nivel_pressao_receptora_medicao = 10*log10(dados_medicao(:, 4)/(2e-5)^2);
-aceleracao_medicao = dados_medicao(:, 6);
+nivel_pressao_geradora_medicao = 20*log10(dados_medicao(:, 2)/(2e-5));
+nivel_pressao_receptora_medicao = 20*log10(dados_medicao(:, 4)/(2e-5));
+aceleracao_gravidade = 9.795;
+aceleracao_referencia = 10e-6/aceleracao_gravidade;
+aceleracao_medicao = 20*log10(dados_medicao(:, 6)/aceleracao_referencia);
 % dados de T60
 dados_T_60 = textread('Dados_experimento_5/tempo_reverberacao_receptora.txt');
 T_60 = dados_T_60(:, 2);
@@ -21,9 +23,10 @@ nivel_pressao_geradora_fundo = nivel_pressao_geradora_fundo(find(frequencias==10
 nivel_pressao_receptora_fundo = nivel_pressao_receptora_fundo(find(frequencias==100):find(frequencias==10000));
 nivel_pressao_geradora_medicao = nivel_pressao_geradora_medicao(find(frequencias==100):find(frequencias==10000));
 nivel_pressao_receptora_medicao = nivel_pressao_receptora_medicao(find(frequencias==100):find(frequencias==10000));
+aceleracao_medicao = aceleracao_medicao(find(frequencias==100):find(frequencias==10000));
 frequencias = frequencias(find(frequencias==100):find(frequencias==10000));
 
-volume_camara = 199,10; % [m^3]
+volume_camara = 199.10; % [m^3]
 area_placa = 1.8*1.13; % [m^2]
 
 figure(1);
@@ -72,10 +75,22 @@ axis([1 21 0 100]);
 % de acordo com o trabalho do MARCOS SOUZA LENZI: MODELOS VIBROACÚSTICOS DE MÉDIAS E ALTAS FREQUÊNCIAS DE PAINÉIS AERONÁUTICOS DE COMPÓSITOS
 % para calcular o A usou-se essa referencia:
 
+%% calculo usando o tempo de reverberacao
 coeficiente_absorcao = 0.161*volume_camara./T_60;
-perda_transmissao = abs(nivel_pressao_receptora_medicao - nivel_pressao_geradora_medicao + 10*log10(area_placa./coeficiente_absorcao));
+perda_transmissao_tempo_reverberacao = nivel_pressao_geradora_medicao - ... 
+nivel_pressao_receptora_medicao + 10*log10(area_placa./coeficiente_absorcao);
+%% calculo usando a comparacao da fonte 
+fonte = textread('Dados_experimento_5/fonte.txt');
+frequencias_fonte = fonte(:, 1);
+potencia_fonte = fonte(:, 2);
+potencia_fonte = potencia_fonte(find(frequencias_fonte==100):find(frequencias_fonte==10000));
+nivel_pressao_fonte = fonte(:, 3);
+nivel_pressao_fonte = nivel_pressao_fonte(find(frequencias_fonte==100):find(frequencias_fonte==10000));
+coeficiente_absorcao = 10.^((6.2 + potencia_fonte - nivel_pressao_fonte)/10);
+perda_transmissao_comparacao = nivel_pressao_geradora_medicao - ... 
+nivel_pressao_receptora_medicao + 10*log10(area_placa./coeficiente_absorcao);
 figure(3);
-semilogx(frequencias, perda_transmissao);
+semilogx(frequencias, perda_transmissao_tempo_reverberacao, 'red', frequencias, perda_transmissao_comparacao, 'blue');
 %bar(perda_transmissao, 'green'); hold on;
 %set(gca,'XTick',1:1:21);
 %set(gca,'XTickLabel',{'100' '125' '160' '200' '250', '315','400', '500','630', '800' ,'1k', '1.25k' ,'1.6k', '2k' ,'2.5k',...
@@ -84,5 +99,12 @@ xlabel('Frequencia - [Hz]','FontSize',20);
 ylabel('NPS - [dB]','FontSize',20);
 title('Perda de Transmissao','FontSize',20);
 axis([0 10e3 0 50]);
+legend('Usando o tempo de reverberacao da sala.', 'Usando a potencia e nivel de pressao referencia da fonte (indireto).');
 
+figure(4);
+semilogx(frequencias, aceleracao_medicao, 'green'); hold on;
+xlabel('Frequencia - [Hz]','FontSize',20);
+ylabel('G - [dB]','FontSize',20);
+title('Sinal do Acelerometro','FontSize',20);
+%axis([1 21 0 100]);
 % plotar os graficos
